33edc93827ffcf54d2916309765a7963bd6925b2
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-04 16:09:57 +0600

    import/export advanced

---
+++ export.js
@@ -0,0 +1,9 @@
+//  named export
+export const name = "Jagadish Chakma";
+export const age = 23;
+
+function getName() {
+    return "Namita Chakma";
+}
+
+export default getName;
---
+++ import.js
@@ -0,0 +1,3 @@
+// named import
+import Gf, { age, name } from "./export.js";
+console.log(Gf(), name, age);
