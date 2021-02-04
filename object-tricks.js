61444b2636de5deca9db61c6031cc7ba159de51f
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-04 13:10:16 +0600

    some object tricks that very usefull

---
+++ object-tricks.js
@@ -0,0 +1,13 @@
+const person = {
+    name: 'Jagadish Chakma',
+    age: 23,
+    study: 'BBA',
+    country: 'Bangladesh'
+}
+// Object.keys() method
+const tricks1 = Object.keys(person);
+console.log(tricks1);
+const tricks2 = Object.values(person);
+console.log(tricks2);
+const tricks3 = Object.entries(person);
+console.log(tricks3)
--- push.js
+++ push.js
@@ -3,4 +3,4 @@ const roll = [21,  34, 43, 46, 456];
 const result = roll.push(23, 453, 432);
 
 console.log(roll);
-console.log(result);
+console.log(result); 
