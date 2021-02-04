33146bf0b38bed369f2a42396490efd665f75cb2
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-04 08:00:21 +0600

    ternary operator usecase

---
+++ ternary.js
@@ -0,0 +1,20 @@
+// a simple condition
+const age = 12;
+
+if(age >= 18){
+    console.log('You are corect');
+}else{
+    console.log('You are not eligible');
+}
+
+// it's simplified using ternary operator
+const check = (age >= 18) ? 'oook' : 'Not';
+console.log(check);
+
+
+// simplifies true false outcome
+const roll = 10;
+
+const out = roll > 5;
+
+console.log(out);
