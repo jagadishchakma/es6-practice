4de12fed76351de3f13f79b93b937019ab487327
jagadishchakma <jagadishchakma401@gmail.com>
2021-02-04 11:21:37 +0600

    slice and splice method usecase in different systemes or ways

---
+++ slice.js
@@ -0,0 +1,8 @@
+// get between result value 
+const roll = [1, 2, 3, 4, 5, 6];
+const result = roll.slice(2, 5);
+console.log(result);
+
+// get defalut result
+const result2 = roll.slice();
+console.log(result2);
---
+++ splice.js
@@ -0,0 +1,6 @@
+// splice() usecase 
+
+const roll= [1, 2, 3, 4, 5, 6, 7, 8];
+const result = roll.splice(2, 2, 34, 34);
+console.log(roll);
+console.log(result);
